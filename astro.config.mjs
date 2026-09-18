9764a8c75ca8f1f911376253ec89e90cdb3f94aa
Paul Bacchus <psbgit@proton.me>
2026-09-18 16:14:53 +0100

    Changed fonts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,23 +15,18 @@ export default defineConfig({
   fonts: [
     {
       provider: fontProviders.local(),
-      name: "IBMPlexSans",
-      cssVariable: "--font-ibm-plex-sans",
+      name: "InterVariable",
+      cssVariable: "--font-inter-variable",
       options: {
         variants: [
           {
-            src: ["./src/assets/fonts/IBMPlexSans-Regular.woff2"],
-            weight: 400,
+            src: ["./src/assets/fonts/InterVariable.woff2"],
+            weight: "400 500 600 700 800 900",
             style: "normal"
           },
           {
-            src: ["./src/assets/fonts/IBMPlexSans-Bold.woff2"],
-            weight: 700,
-            style: "oblique"
-          },
-          {
-            src: ["./src/assets/fonts/IBMPlexSans-Italic.woff2"],
-            weight: 400,
+            src: ["./src/assets/fonts/InterVariable-Italic.woff2"],
+            weight: "400 500 600 700 800 900",
             style: "italic"
           },
         ]
@@ -39,158 +34,33 @@ export default defineConfig({
     },
     {
       provider: fontProviders.local(),
-      name: "NebulaSans",
-      cssVariable: "--font-nebula-sans",
+      name: "JetBrainsMonoVariable",
+      cssVariable: "--font-jetbrains-mono-variable",
       options: {
         variants: [
           {
-            src: ["./src/assets/fonts/NebulaSans-Book.woff2"],
-            weight: 400,
+            src: ["./src/assets/fonts/JetBrainsMono[wght].ttf"],
+            weight: "400 500 600 700 800 900",
             style: "normal"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-BookItalic.woff2"],
-            weight: 400,
-            style: "italic"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-Medium.woff2"],
-            weight: 500,
-            style: "normal"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-MediumItalic.woff2"],
-            weight: 500,
-            style: "italic"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-Semibold.woff2"],
-            weight: 600,
-            style: "normal"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-SemiboldItalic.woff2"],
-            weight: 600,
-            style: "italic"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-Bold.woff2"],
-            weight: 700,
-            style: "normal"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-BoldItalic.woff2"],
-            weight: 700,
-            style: "italic"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-Black.woff2"],
-            weight: 900,
-            style: "normal"
-          },
-          {
-            src: ["./src/assets/fonts/NebulaSans-BlackItalic.woff2"],
-            weight: 900,
-            style: "italic"
           },
         ]
       }
     },
     {
       provider: fontProviders.local(),
-      name: "JetBrainsMono",
-      cssVariable: "--font-jet-brains-mono",
+      name: "JetBrainsMonoRegular",
+      cssVariable: "--font-jetbrains-mono-regular",
       options: {
         variants: [
           {
             src: ["./src/assets/fonts/JetBrainsMono-Regular.woff2"],
-            weight: 400,
+            weight: "400",
             style: "normal"
           },
         ]
       }
     },
-    {
-      provider: fontProviders.local(),
-      name: "Geist",
-      cssVariable: "--font-geist",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/Geist[wght].woff2"],
-            weight: "400 500 700 900",
-            style: "normal"
-          },
-          {
-            src: ["./src/assets/fonts/Geist-Italic[wght].woff2"],
-            weight: "400 500 700 900",
-            style: "italic"
-          },
-        ]
-      }
-    },
-    {
-      provider: fontProviders.local(),
-      name: "Geist",
-      cssVariable: "--font-geist-mono",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/GeistMono-Regular.woff2"],
-            weight: 400,
-            style: "normal"
-          },
-        ]
-      }
-    },
-    {
-      provider: fontProviders.local(),
-      name: "Geist",
-      cssVariable: "--font-geist-pixel-circle",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/GeistPixel-Circle.woff2"],
-            weight: 400,
-            style: "normal"
-          },
-        ]
-      }
-    },
-    {
-      provider: fontProviders.local(),
-      name: "Caveat",
-      cssVariable: "--font-caveat",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/caveat-regular-webfont.woff"],
-            weight: 400,
-            style: "normal"
-          },
-          {
-            src: ["./src/assets/fonts/caveat-bold-webfont.woff"],
-            weight: 700,
-            style: "normal"
-          },
-        ]
-      }
-    },
-    {
-      provider: fontProviders.local(),
-      name: "ShantellSans",
-      cssVariable: "--font-shantell",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/ShantellSans[BNCE,INFM,SPAC,ital,wght].woff2"],
-            weight: "400 700 900",
-            style: "normal"
-          },
 
-        ]
-      }
-    },
   ],
 
   // vite: {
